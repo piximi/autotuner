929fcea2c28ff4137cc9f48332fa0f43ebce9917
moserle <moserle@student.ethz.ch>
2019-07-15 11:44:30 +0200

    add proper types to priors

--- src/priors.ts
+++ src/priors.ts
@@ -2,33 +2,32 @@ import equal from 'deep-equal';
 import * as math from 'mathjs';
 
 class Priors {
-    domain: number[];
-    observedValues: any;
-    // TODO: type mean and kernel properly
-    mean: any;
-    kernel: any;
+    domainIndices: number[];
+    // 'observedValues': mapping from a point to its observed values
+    observedValues: { [identifier: string]: number[]};
+    mean: number[];
+    kernel: math.Matrix;
 
-    constructor (domain: number[]) {
-    this.domain = domain
-    this.observedValues = {}
+    constructor (domainIndices: number[]) {
+        this.domainIndices = domainIndices
+        this.observedValues = {}
 
-    var domainSize: number = Object.keys(this.domain).length + 1;
-    for (var i=0; i < domainSize; i++) {
-        this.observedValues[domain[i]] = [];
+        var domainSize: number = Object.keys(this.domainIndices).length + 1;
+        for (var i=0; i < domainSize; i++) {
+            this.observedValues[domainIndices[i]] = [];
+        }
+        this.mean = math.zeros(domainSize) as number[];
+        this.kernel = math.identity(domainSize) as math.Matrix;
     }
-    this.mean = math.zeros(domainSize);
-    this.kernel = math.identity(domainSize);
-    }
 
-    // TODO: create type for observedValues
-    commit (observedValues: any) {
-        var domainSize: number = Object.keys(this.domain).length + 1;
+    commit (observedValues: {[identifier: number]: number}) {
+        var domainSize: number = Object.keys(this.domainIndices).length + 1;
 
         for (var point in observedValues) {
             this.observedValues[point].push(observedValues[point]);
 
             // Find domain index.
-            var idx = this.domain.findIndex((x: number) => equal(x, point));
+            var idx = this.domainIndices.findIndex((x: number) => equal(x, point));
 
             // Recompute the mean.
             this.mean[idx] = math.mean(this.observedValues[point]);
@@ -44,28 +43,29 @@ class Priors {
             }
         }
         for (var i = 0; i < domainSize; i++) {
-            if (this.observedValues[this.domain[i]].length === 0) {
+            if (this.observedValues[this.domainIndices[i]].length === 0) {
                 this.mean[i] = sum / count;
             }
         }
 
         // Recompute the kernel by using the standard covariance function between all observed points.
-        for (var point in observedValues) {
-            var idx = this.domain.findIndex((x: number) => equal(x, point));
-            for (var point2 in observedValues) {
+        for (let point in observedValues) {
+            var idx: number = this.domainIndices.findIndex((x: number) => equal(x, point)) + 1;
+            for (let point2 in observedValues) {
                 if (this.observedValues[point2].length > 0){
-                    var idx2 = this.domain.findIndex((x: number) => equal(x, point2));
-                    var cov = 0.0;
-                    for (var i = 0; i < this.observedValues[point].length; i++) {
-                        for (var j = 0; j < this.observedValues[point2].length; j++) {
+                    var idx2: number = this.domainIndices.findIndex((x: number) => equal(x, point2)) + 1;
+                    var cov: number = 0.0;
+                    for (let i: number = 0; i < this.observedValues[point].length; i++) {
+                        for (let j: number = 0; j < this.observedValues[point2].length; j++) {
                             if (i <= j) {
                                 cov += (this.observedValues[point][i] - this.mean[idx]) * (this.observedValues[point2][j] - this.mean[idx2]);
                             }
                         }
                     }
+                    // TODO: test matrix operations and casts to matrix
                     cov /= (this.observedValues[point].length * this.observedValues[point2].length)
-                    this.kernel[idx][idx2] = cov;
-                    this.kernel[idx2][idx] = cov;
+                    this.kernel = math.subset(this.kernel, math.index(idx, idx2), cov) as math.Matrix;
+                    this.kernel = math.subset(this.kernel, math.index(idx2, idx), cov) as math.Matrix;
                 }
             }
         }
